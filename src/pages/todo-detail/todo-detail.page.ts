6f0aa4454d284bfde20c1a194404b6f55388ad4e
Erik August Johnson <erik@erikaugust.com>
2017-05-13 09:07:28 -0600

    Adds edit detail functionality

--- src/pages/todo-detail/todo-detail.page.ts
+++ src/pages/todo-detail/todo-detail.page.ts
@@ -3,6 +3,8 @@ import { NavParams } from 'ionic-angular';
 
 import { Todo } from '../../core/models/todo.model';
 
+import { TodosService } from '../../core/services/todos.service';
+
 @Component({
   selector: 'page-todo-detail',
   templateUrl: 'todo-detail.page.html'
@@ -14,15 +16,38 @@ import { Todo } from '../../core/models/todo.model';
 export class TodoDetailPage {
 
   public todo: Todo = new Todo('', '');
+  public editMode: boolean = false;
 
-  constructor(public navParams: NavParams) {}
+  constructor(public navParams: NavParams, private todosService: TodosService) {}
 
   /**
    * On load, get todo data from navigation parameters
    * @name ionViewDidLoad
    */
   ionViewDidLoad() {
-    this.todo = this.navParams.data;
+    this.todo = this.navParams.data.items[this.navParams.data.index];
+  }
+
+  /**
+   * Saves changes made to item
+   * @name saveChanges
+   */
+  public saveChanges(): void {
+    let index = this.navParams.data.index;
+    let items = this.navParams.data.items;
+
+    items[index] = this.todo;
+
+    this.todosService.save(items);
+    this.toggleEdit();
+  }
+
+  /**
+   * Toggles edit mode boolean
+   * @name toggleEdit
+   */
+  public toggleEdit(): void {
+    this.editMode = !this.editMode;
   }
 
 }
